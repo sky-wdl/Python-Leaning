ab9938c7e44ae9dbe5c5db31832bf949f149adff
文东柳/Dongliu Wen <gtxywdl@sina.com>
2022-09-28 15:13:17 +0800

    更新延大的试题答案，增加题目译文大致意思

--- demo.py
+++ demo.py
@@ -1,7 +1,36 @@
-def cmul(a, *b):
-    m = a
-    for i in b:
-        m *= i
-    return m
+words = ['The', 'LORD', 'is', 'my', 'shepherd', 'I', 'shall', 'not', 'be', 'in', 'want', 'He', 'makes', 'me', 'lie',
+         'down', 'in', 'green', 'pastures', 'he', 'leads', 'me', 'beside', 'quiet', 'waters', 'he', 'restores', 'my',
+         'soul', 'He', 'guides', 'me', 'in', 'paths', 'of', 'righteousness', 'for', 'his', 'names', 'sake', 'Even',
+         'though', 'I', 'walk', 'through', 'the', 'valley', 'of', 'the', 'shadow', 'of', 'death', 'I', 'will', 'fear',
+         'no', 'evil', 'for', 'you', 'are', 'with', 'me', 'your', 'rod', 'and', 'your', 'staff', 'they', 'comfort',
+         'me', 'You', 'prepare', 'a', 'table', 'before', 'me', 'in', 'the', 'presence', 'of', 'my', 'enemies', 'You',
+         'anoint', 'my', 'head', 'with', 'oil', 'my', 'cup', 'overflows', 'Surely', 'goodness', 'and', 'love', 'will',
+         'follow', 'me', 'all', 'the', 'days', 'of', 'my', 'life', 'and', 'I', 'will', 'dwell', 'in', 'the', 'house',
+         'of', 'the', 'LORD', 'forever']
 
-print(eval("cmul({})".format(input())))
+a = {}
+for i in words:
+    if a. __contains__(i):
+        pass
+    else:
+        a[i] = 1
+b = []
+i = 0
+for Key, value in a.items():
+    if i == 10:
+        break
+    else:
+        for s in range(value):
+            b.append(Key)
+        i += 1
+print(b)
+
+
+# a = set()
+# for i in words:
+#     a.add(i)
+# a = list(a)
+# b =[]
+# for i in range(10):
+#     b.append(a[i])
+# print(b)
--- 韩国延世大学选修课作业9.27-1.py
+++ 韩国延世大学选修课作业9.27-1.py
@@ -1,3 +1,9 @@
+"""
+译文大致意思：
+按照list的形式输出包含十个不重复的字符，该十个字符的排列顺序按照words list中的先后出现顺序进行排列。
+"""
+
+
 words = ['The', 'LORD', 'is', 'my', 'shepherd', 'I', 'shall', 'not', 'be', 'in', 'want', 'He', 'makes', 'me', 'lie',
          'down', 'in', 'green', 'pastures', 'he', 'leads', 'me', 'beside', 'quiet', 'waters', 'he', 'restores', 'my',
          'soul', 'He', 'guides', 'me', 'in', 'paths', 'of', 'righteousness', 'for', 'his', 'names', 'sake', 'Even',
@@ -7,11 +13,18 @@ words = ['The', 'LORD', 'is', 'my', 'shepherd', 'I', 'shall', 'not', 'be', 'in',
          'anoint', 'my', 'head', 'with', 'oil', 'my', 'cup', 'overflows', 'Surely', 'goodness', 'and', 'love', 'will',
          'follow', 'me', 'all', 'the', 'days', 'of', 'my', 'life', 'and', 'I', 'will', 'dwell', 'in', 'the', 'house',
          'of', 'the', 'LORD', 'forever']
-a = set()
+a = {}
 for i in words:
-    a.add(i)
-a = list(a)
-for i in range(10):
-    print(a[i])
-
-
+    if a. __contains__(i):
+        a[i] = a[i] + 1
+    else:
+        a[i] = 1
+b = []
+i = 0
+for Key, value in a.items():
+    if i == 10:
+        break
+    else:
+        b.append(Key)
+        i += 1
+print(b)
