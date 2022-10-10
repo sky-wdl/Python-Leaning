c3cfa2ca20057c869881cf161f868bdee1511333
文东柳/Dongliu Wen <gtxywdl@sina.com>
2022-10-10 20:11:44 +0800

    更正，把+改为-

--- 韩国延世大学选修课作业10.10-2.py
+++ 韩国延世大学选修课作业10.10-2.py
@@ -14,8 +14,8 @@ def get_chosun_name(month, day):
     name2 = ['자', '실', '무', '숙', '분', '봉', '년', '놈', '석', '식', '걸', '룡', '순', '나', '창', '란', '렬', '실', '경', '종', '덕',
              '득', '복', '동', '꽁', '중', '신', '미', '오', '욕', '님']
 
-    if (not (m>12 or m<1)) and (not (d>31 or d<1)) :
-        return name1[month+1]+name2[day+1]
+    if (not (m > 12 or m < 1)) and (not (d > 31 or d < 1)):
+        return name1[month - 1] + name2[day - 1]
     else:
         return False
 
@@ -24,6 +24,6 @@ m = eval(input('태어난 월>'))
 d = eval(input('태어난 일>'))
 
 if get_chosun_name(m, d):
-    print('당신의 조선시대 이름은: '+get_chosun_name(m, d))
+    print('당신의 조선시대 이름은: ' + get_chosun_name(m, d))
 else:
     print('잘못된 입력')
