3f9c41b6e93ffa7919ee628f19b912c5f30f2c28
文东柳/Dongliu Wen <gtxywdl@sina.com>
2022-10-10 21:36:54 +0800

    更正，bmi计算错误，区间判断错误

--- 韩国延世大学选修课作业10.10-1.py
+++ 韩国延世大学选修课作业10.10-1.py
@@ -2,24 +2,24 @@
 题目译文大意：
 请编写输入体重和身高后能输出当前体重状态的程序。
 定义/使用以下两个函数:
-Bmi() - 输入体重和身高后输出身体的BMI指数
+bmi() - 输入体重和身高后输出身体的BMI指数
 weight_status() - 输入BMI指数后返回到weight statu
 
 """
 
 
 def bmi(weight, height):
-    weight_status((weight / height ** 2) * (0.1 ** 4))
+    weight_status((weight / (height*0.01) ** 2))
 
 
-def weight_status(Bmi):
-    if Bmi > 30.0:
+def weight_status(bmi):
+    if bmi > 30.0:
         print("Weight status: [Obses]")
-    elif 25.0 < Bmi <= 30.0:
+    elif 25.0 < bmi <= 30.0:
         print("Weight status: [Overweight]")
-    elif 18.0 <= Bmi <= 25.0:
+    elif 18.5 <= bmi <= 25.0:
         print("Weight status: [Normal]")
-    elif Bmi <= 18.5:
+    elif bmi < 18.5:
         print("Weight status: [Underweight]")
 
 
